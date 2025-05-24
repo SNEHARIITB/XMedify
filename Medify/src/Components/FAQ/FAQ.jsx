fd33c54c35fd142e5098a8ffdc225416201e9c43
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-24 12:44:52 +0530

    Update FAQ.jsx

--- Medify/src/Components/FAQ/FAQ.jsx
+++ Medify/src/Components/FAQ/FAQ.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './faq.module.css';
+import styles from './FAQ.module.css';
 import { Accordion, AccordionSummary, AccordionDetails, Typography, Container, Box } from '@mui/material';
 
 import doctor from "../../assets/doctor-patient.jpg";
